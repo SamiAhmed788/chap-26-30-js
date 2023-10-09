fd3ca7ed3371e2dc86bea4372f4062ab68e2206b
cricket lover <sami76748@gmail.com>
2023-10-09 07:53:26 -0700

    class work

---
+++ script.js
@@ -0,0 +1,87 @@
+
+// q no 1
+// let number = prompt(`give me  positive integer number `)
+//  number = Number(number)
+
+// if (isNaN(number)|| number<= 0) {
+//     alert(`please Enter positive integer number`)
+// }else{
+//  document.write("Number: " + number + "<br>");
+//   document.write("Round off value: " + Math.round(number) + "<br>");
+//   document.write("Floor value: " + Math.floor(number) + "<br>");
+//   document.write("Ceil  wali value value: " + Math.ceil(number) + "<br>");
+// }
+
+
+// qno 2
+// let number = prompt(`give me negative integer number `)
+//  number = Number(number)
+
+// if (number>=-0 ) {
+//     alert(`please Enter negative integer number`)
+
+// }else{
+//  document.write("Number:" +number + "<br>");
+//   document.write("Round off value:-" + Math.round(number) + "<br>");
+//   document.write("Floor value:" + Math.floor(number) + "<br>");
+//   document.write("Ceil  wali value value:" + Math.ceil(number) + "<br>");
+// }
+// // qno 3
+// let number = prompt(`enter a number `)
+//  number = Number(number)
+
+// if (isNaN(number) ) {
+//     alert(`invalid input please write correct input`)
+
+// }else{
+//  document.write("absoulote value is" +number + "is" + Math.abs(number) +"<br>");}
+
+// qno 4
+//  var dice = Math.floor(Math.cl() * 6) + 1;
+//  document.write("you got "+ dice + "cograts")
+// qno5
+// var coin = Math.random()
+
+// if (coin > 0.5) {
+//     document.write("heads <br> " )
+// } 
+
+// else {
+//     document.write("tail <br> " )
+// }
+// qno6
+
+//  var dice = Math.floor(Math.random() * 100) + 1;
+
+//  document.write("The random number is " + dice + "<br>");
+
+
+//
+// qno7
+// var userinput = prompt("whats your weight buddy")
+
+// var weight = parseFloat(userinput)
+
+// if (isNaN(weight)) {
+//     document.write("Invalid input.");
+// }else{
+//     document.write("Your weight is " + weight + " kilograms.")
+// }
+// q no8
+
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// var userInput = prompt("Please enter a number between 1 and 10");
+
+// var userNumber = parseInt(userInput);
+// if (isNaN(userNumber) || userNumber < 1 || userNumber > 10) {
+ 
+//   alert("1 sa lakar 10 tk koi bhi number guess karay.");
+// } else {
+
+//   if (userNumber === secretNumber) {
+//     document.write("javab bilkul durust hai ");
+//   } else {
+//     document.write("galat javab sahi <BR> javab ya hai " + secretNumber + ".");
+//   }
+// }
